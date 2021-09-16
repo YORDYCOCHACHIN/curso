5eb171693fa41f6ff8ac26e287b7002d61122525
Yordy Cochachin <eldiyor1996@gmail.com>
2021-09-16 11:59:31 -0500

    Iniciando el proyecto

---
+++ course.go
@@ -0,0 +1,24 @@
+package cursito
+
+import "fmt"
+
+type course struct {
+	name    string
+	Price   float64
+	isfree  bool
+	userId  []uint
+	classes map[uint]string
+}
+
+func (c course) imprimirclase() {
+	text := "Las clases son: "
+	for _, class := range c.classes {
+		text += class + " ,"
+	}
+	fmt.Println(text[:len(text)-2])
+}
+
+func (c *course) Actualizarprecio(price float64) {
+	c.Price = price
+
+}
